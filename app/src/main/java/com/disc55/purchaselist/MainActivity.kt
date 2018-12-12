d13b5d9a4cb2d596701d981904a02bf31a504d20
Disc <patiya.k@gmail.com>
2018-12-13 00:32:29 +0700

    test uploading

--- app/src/main/java/com/disc55/purchaselist/MainActivity.kt
+++ app/src/main/java/com/disc55/purchaselist/MainActivity.kt
@@ -21,36 +21,45 @@ public class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        db = FirebaseFirestore.getInstance().document("user2/location") //edit database under this path
+        //db = FirebaseFirestore.getInstance().document("user2/location") //edit database under this path
+        db = FirebaseFirestore.getInstance().document("Users/Disc") //edit database under this path
 
-        val save = findViewById<View>(R.id.saveBtn) as Button
+        val save = findViewById<View>(R.id.add_purchase_item_btn) as Button
         save.setOnClickListener {
-            view: View? -> save()
+                view: View? -> save()
         }
 
     }
 
     private fun save() {
 
+        val itemname_edit_txt = findViewById<View>(R.id.itemname_edit_txt) as EditText
+        val quantity_edit_txt = findViewById<View>(R.id.quantity_edit_txt) as EditText
+        val unit_edit_txt = findViewById<View>(R.id.quantity_edit_txt) as EditText
+        val location_edit_txt = findViewById<View>(R.id.location_edit_txt) as EditText
 
-        val edit_text_title = findViewById<View>(R.id.edit_text_title) as EditText
-        val edit_text_description = findViewById<View>(R.id.edit_text_description) as EditText
+        val itemname = itemname_edit_txt.text.toString().trim()
+        val quantity = quantity_edit_txt.text.toString().trim()
+        val unit = quantity_edit_txt.text.toString().trim()
+        val location = location_edit_txt.text.toString().trim()
 
-
-        val title = edit_text_title.text.toString().trim()
-        val description = edit_text_description.text.toString().trim()
-
-
-        if( (!title.isEmpty()) && (!description.isEmpty()) ){
+        if( (!itemname.isEmpty()) && (!quantity.isEmpty()) ){
             try {
-                Toast.makeText(this,"OK",Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"OK",Toast.LENGTH_SHORT).show()
+
                 val items = HashMap<String, Any>()
-                items.put("title",title)
-                items.put("description",description)
-                db.collection(title).document("BigC").set(items).addOnSuccessListener {
-                    void: Void? ->  Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
+                items.put("itemname",itemname)
+                items.put("quantity",quantity)
+                items.put("unit",unit)
+
+                //generate auto ID of item for every items in the collection
+                //val ref = db.collection("Locations").document(location).collection("Items").document()
+                //val ItemId = ref.id
+                val ItemId = db.collection("Locations").document(location).collection("Items").document().id
+                db.collection("Locations").document(location).collection("Items").document(ItemId).set(items).addOnSuccessListener {
+                        void: Void? ->  Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
                 }.addOnFailureListener {
-                    exception: java.lang.Exception -> Toast.makeText(this,exception.toString(),Toast.LENGTH_LONG).show()
+                        exception: java.lang.Exception -> Toast.makeText(this,exception.toString(),Toast.LENGTH_LONG).show()
                 }
             }catch (e:Exception){
                 Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
@@ -59,6 +68,9 @@ public class MainActivity : AppCompatActivity() {
         else {
             Toast.makeText(this,"Please fill up the fields",Toast.LENGTH_LONG).show()
         }
+
+
+
     }
 
 }
