81661fe347fc9560bb4dadcf0dbd01815a7e72bd
Disc <patiya.k@gmail.com>
2018-12-14 15:32:59 +0700

    test uploading

--- app/src/main/java/com/disc55/purchaselist/MainActivity.kt
+++ app/src/main/java/com/disc55/purchaselist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.disc55.purchaselist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,39 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.container, MainFragment.newInstant())
             .commit()
 
-    }
+        lateinit var db: FirebaseFirestore
+        db = FirebaseFirestore.getInstance()
 
+        val disp = findViewById(R.id.dispTxtTest) as TextView
+
+        //#### Get multiple documents from query from a collection ####/
+
+        //disp.text = "OK"
+        /*db.collection("Users").whereEqualTo("name","Mint")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    disp.text = disp.text.toString() + " /#/ " + document.data["id"]
+                }
+            }
+            .addOnFailureListener { exception ->
+                disp.text = "Error getting documents: " + exception
+            }
+        */
+
+        db.collection("Users").document("Disc")
+            .collection("Locations").document("BigC")
+            .collection("Items")//.whereEqualTo("name","Mint")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    disp.text = disp.text.toString() + " , " + document.data["itemname"]
+                }
+            }
+            .addOnFailureListener { exception ->
+                disp.text = "Error getting documents: " + exception
+            }
+
+    }
 
 }
--- app/src/main/java/com/disc55/purchaselist/MainFragment.kt
+++ app/src/main/java/com/disc55/purchaselist/MainFragment.kt
@@ -32,87 +32,27 @@ class MainFragment: Fragment(), PurchaseListListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val item = arrayListOf(
+            Purchase("ไข่ไก่", 1.0f, "โหล", "open", Timestamp(Date())),
+            Purchase("ไก่", 1.0f, "Kg", "open", Timestamp(Date()))
+        )
+
         //TODO: add firestore
         db = FirebaseFirestore.getInstance()
 
-        /*
-        public fun getQuotesFromFireStore(){
-            db.collection(FirebaseConstants.COLLECTION_PARENT)
-                .get()
-                .addOnCompleteListener(object : OnCompleteListener<QuerySnapshot>) {
-                    override fun onComplete(task: Task<QuerySnapshot>){
-                        if(task.isSuccessful){
-                            for(document in task.result){
-                                if(document.id==FirebaseConstants.DOCUMENT_PARENT){
-                                    document.data[FirebaseConstants.]
-                                }
-                                Log.d(TAG,document.id)
-                            }
-                        }else{
+        item.add(Purchase("หมู", 2.0f, "Kg", "open", Timestamp(Date())))
 
-                        }
-                    }
-                }
-        }
-*/
-        //val disp: TextView = findViewById(R.id.dispTxt) as TextView
-        val disp = R.id.dispTxt as TextView
-
-        //Get Data in a Collection and a Document
-        disp.text = "OK"
-        //disp.setText("OK")
-        /*val docRef = db.collection("Users").document("Disc")
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    //Log.d(TAG, "DocumentSnapshot data: " + document.data)
-                    disp.text = document.data.toString()
-
-
-                } else {
-                    //Log.d(TAG, "No such document")
-                    disp.text = "No such document"
-                }
-            }
-            .addOnFailureListener { exception ->
-                //Log.d(TAG, "get failed with ", exception)
-                disp.text = "get failed with"
-            }
-        */
-
-        //Get all documents in a collection
-        /*disp.text = "OK"
-        db.collection("Users")
+        db.collection("Users").document("Disc")
+            .collection("Locations").document("BigC")
+            .collection("Items")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    disp.text = disp.text.toString() + " /#/ " + document.data
+                    item.add(Purchase(document.data["itemname"].toString(),2.0f,"Kg","open", Timestamp(Date())))
                 }
             }
             .addOnFailureListener { exception ->
-                disp.text = "Error getting documents: " + exception
             }
-        */
-
-        //Get multiple documents from query from a collection
-        /*disp.text = "OK"
-        db.collection("Users").whereEqualTo("name","Mint")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    disp.text = disp.text.toString() + " /#/ " + document.data
-                }
-            }
-            .addOnFailureListener { exception ->
-                disp.text = "Error getting documents: " + exception
-            }
-        */
-
-
-        val item = arrayListOf(
-            Purchase("ไข่ไก่", 1.0f, "โหล", "open", Timestamp(Date())),
-            Purchase("ไข่ไก่", 1.0f, "โหล", "open", Timestamp(Date()))
-        )
 
         val listAdapter = PurchaseListAdapter(item, this)
 
