83ae0e44d07600a15513d5fd16b0b8f4a1e8a833
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2018-12-14 11:54:44 +0700

    remove code for login which cannot build app

--- app/src/main/java/com/disc55/purchaselist/LoginFragment.kt
+++ app/src/main/java/com/disc55/purchaselist/LoginFragment.kt
@@ -5,27 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_login.*
-import com.google.android.gms.auth.api.Auth
-import android.content.Intent
-import android.util.Log
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-
 
 class LoginFragment: Fragment() {
-
-    private val RC_SIGN_IN = 9001
-    private val TAG = "PurchaseList"
-
-    lateinit var mGoogleApiClient: GoogleApiClient
-
-    private lateinit var auth: FirebaseAuth
 
     companion object {
         fun newInstant() = LoginFragment()
@@ -37,23 +18,6 @@ class LoginFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-//        mGoogleApiClient = GoogleApiClient.Builder(context!!)
-//            .enableAutoManage(activity, context!!)
-//            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-//            .build()
-
-        auth = FirebaseAuth.getInstance()
-
-        signInButton.setOnClickListener {
-            val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        }
 
     }
 
